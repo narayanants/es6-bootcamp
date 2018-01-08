e96037573e9dc4b90f67e19f9585f78781614b54
Narayanan Ts <tsnarayanan7@gmail.com>
2018-01-08 08:27:15 +0530

    const

---
+++ 7 const and Let/const.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Const:
+ * -------
+ * 
+ * 
+ */
+
+// var name = 'Jane',
+//     title = 'Software  Engineer',
+//     hourlyWage = 40;
+
+// ES6
+const name = 'Jane';
+let title = 'Software Engineer',
+    hourlyWage = 42;
